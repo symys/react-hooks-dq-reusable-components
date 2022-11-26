0fc894f7851e93107038c2d5026c73b85476ca7c
SeymaDemir <seymademir35@gmail.com>
2022-11-26 16:09:14 +0300

    it was hard

--- src/components/InventoryTile.js
+++ src/components/InventoryTile.js
@@ -1,16 +1,13 @@
 import React from "react";
+import PotionTile from "./PotionTile";
 
 // Study me! What information do I require to work?
 // How am I the same as ShopTile? How am I different?
 
 function InventoryTile({ potion, sell, children }) {
+
   return (
-    <div className="inventory-card">
-      <div onClick={() => sell(potion.id)} className="image-wrapper">
-        <img className="image" alt={potion.name} src={potion.image_url} />
-      </div>
-      {children}
-    </div>
+    <PotionTile potion={potion} onClick={sell} children={children} className={"inventory-card"}/>
   );
 }
 
--- src/components/ShopTile.js
+++ src/components/ShopTile.js
@@ -1,16 +1,13 @@
 import React from "react";
+import PotionTile from "./PotionTile";
 
 // Study me! What information do I require to work?
 // How am I the same as InventoryTile? How am I different?
 
 function ShopTile({ potion, addToInventory, children }) {
+ 
   return (
-    <div className="card">
-      <div onClick={() => addToInventory(potion.id)} className="image-wrapper">
-        <img className="image" alt={potion.name} src={potion.image_url} />
-      </div>
-      {children}
-    </div>
+    <PotionTile className={"card"} potion={potion} onClick={addToInventory} children={children}/>
   );
 }
 
